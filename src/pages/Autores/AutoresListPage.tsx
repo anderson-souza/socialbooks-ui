03420f7a441956ef0325c5d1801559b906726f57
Anderson <andersonpds14@gmail.com>
2020-07-24 20:02:01 -0300

    Corrige redirecionamento da Home Page; Adiciona funcionalidade de exclusão de Autor;

--- src/pages/Autores/AutoresListPage.tsx
+++ src/pages/Autores/AutoresListPage.tsx
@@ -4,6 +4,8 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 import api from "../../service/api";
 import { useState, useEffect } from "react";
+import DialogConfirmacao from "../../components/DialogConfirmacao";
+import { deletarAutor } from "../../service/AutorService";
 
 export default function AutoresListPage() {
   const [autores, setAutores] = useState([]);
@@ -25,33 +27,60 @@ export default function AutoresListPage() {
     loadAutores();
   }, []);
 
+  const onClickDeleteAutor = (id: string) => {
+    DialogConfirmacao(
+      "Atenção",
+      "Deseja Excluir o Autor?",
+      "warning",
+      "Excluir",
+      () => {
+        deletarAutor(id)
+          .then(() => {
+            setAutores(autores.filter((autores) => autores["id"] !== id));
+            alert("Sucesso viado");
+          })
+          .catch((error) => console.log(error));
+      }
+    );
+  };
+
   return (
-    <Table responsive striped bordered hover>
-      <thead>
-        <tr>
-          <td>Nome</td>
-          <td>Nascimento</td>
-          <td>Nacionalidade</td>
-          <td>Ações</td>
-        </tr>
-      </thead>
-      <tbody>
-        {autores?.map((row: any) => (
-          <tr key={row.id}>
-            <td>{row.nome}</td>
-            <td>{row.nascimento}</td>
-            <td>{row.nacionalidade}</td>
-            <td>
-              <ButtonGroup>
-                <Button variant='warning' href={`autores/${row.id}`}>
-                  Editar
-                </Button>
-                <Button variant='danger'>Excluir</Button>
-              </ButtonGroup>
-            </td>
+    <>
+      <Button variant='success' href={`autores/NEW`}>
+        Novo Autor
+      </Button>
+
+      <Table responsive striped bordered hover>
+        <thead style={{ paddingTop: 20 }}>
+          <tr>
+            <td>Nome</td>
+            <td>Nascimento</td>
+            <td>Nacionalidade</td>
+            <td>Ações</td>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {autores?.map((row: any) => (
+            <tr key={row.id}>
+              <td>{row.nome}</td>
+              <td>{row.nascimento}</td>
+              <td>{row.nacionalidade}</td>
+              <td>
+                <ButtonGroup>
+                  <Button variant='warning' href={`autores/${row.id}`}>
+                    Editar
+                  </Button>
+                  <Button
+                    variant='danger'
+                    onClick={() => onClickDeleteAutor(row.id)}>
+                    Excluir
+                  </Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 }
